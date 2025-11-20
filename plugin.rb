76af6dd4a939f4f5043b0f07a4978df005c88662
mitchlinDEV <35628881+mitchlinDEV@users.noreply.github.com>
2025-11-20 21:46:06 +0100

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# name: custom-pages
+# about: Custom static pages for your Discourse instance (starting with a simple template)
+# version: 0.0.1
+# authors: Richard
+# url: https://github.com/mitchlinDEV/custom-pages
+
+after_initialize do
+  # For now this plugin does nothing. In the next steps we will:
+  # - add a static page route
+  # - add a template that you can duplicate for future pages
+end
